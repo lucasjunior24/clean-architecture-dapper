9bee4506fb052926fffda94afd02219d579191b7
Souza <lucas11souza97@gmail.com>
2026-02-21 00:10:15 -0400

    feat: created project

---
+++ Application/Interfaces/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using DapperCrud.Domain.Entities;
+
+namespace DapperCrud.Application.Interfaces;
+
+public interface IProductRepository
+{
+    Task<IEnumerable<Product>> GetAllAsync();
+    Task<Product?> GetByIdAsync(int id);
+    Task<int> AddAsync(Product product);
+    Task<bool> UpdateAsync(Product product);
+    Task<bool> DeleteAsync(int id);
+}
---
+++ Infrastructure/Repositories/ProductRepository.cs
@@ -0,0 +1,70 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using System.Data;
+using DapperCrud.Application.Interfaces;
+using DapperCrud.Domain.Entities;
+
+namespace DapperCrud.Infrastructure.Repositories;
+
+public class ProductRepository : IProductRepository
+{
+    private readonly string _connectionString;
+
+    public ProductRepository(IConfiguration configuration)
+    {
+        _connectionString = configuration.GetConnectionString("DefaultConnection")!;
+    }
+
+    private IDbConnection Connection => new SqlConnection(_connectionString);
+
+    public async Task<IEnumerable<Product>> GetAllAsync()
+    {
+        using var db = Connection;
+        var data =  await db.QueryAsync<Product>("SELECT * FROM Products");
+        return data;
+    }
+
+    public async Task<Product?> GetByIdAsync(int id)
+    {
+        using var db = Connection;
+        return await db.QueryFirstOrDefaultAsync<Product>(
+            "SELECT * FROM Products WHERE Id = @Id",
+            new { Id = id });
+    }
+
+    public async Task<int> AddAsync(Product product)
+    {
+        using var db = Connection;
+
+        var sql = @"
+            INSERT INTO Products (Name, Price, Stock)
+            VALUES (@Name, @Price, @Stock);
+            SELECT CAST(SCOPE_IDENTITY() as int);
+        ";
+
+        return await db.ExecuteScalarAsync<int>(sql, product);
+    }
+
+    public async Task<bool> UpdateAsync(Product product)
+    {
+        using var db = Connection;
+
+        var sql = @"
+            UPDATE Products
+            SET Name = @Name,
+                Price = @Price,
+                Stock = @Stock
+            WHERE Id = @Id
+        ";
+
+        return await db.ExecuteAsync(sql, product) > 0;
+    }
+
+    public async Task<bool> DeleteAsync(int id)
+    {
+        using var db = Connection;
+        return await db.ExecuteAsync(
+            "DELETE FROM Products WHERE Id = @Id",
+            new { Id = id }) > 0;
+    }
+}
